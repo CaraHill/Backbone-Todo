fe9d102108b5f60b81afbb6b764167574685b5e5
Cara Hill <chilly23@gmail.com>
2015-08-10 09:56:56 +1200

    Changed routes and created root home index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'todos_controller/index'
+  root 'home#index'
 
-  root 'todos#index'
+  resources :todos, only: [:index]
 
   # The priority is based upon order of creation: first
   # created -> highest priority.
