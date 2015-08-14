6db575c2bcde8412b38439f7fce1ff28553e3369
Cara Hill <chilly23@gmail.com>
2015-08-14 12:13:30 +1200

    Added tests for todo_controller.

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe TodosController, type: :controller do
+  before do
+    @todo = Todo.create(description: 'Go to the store to get milk and eggs.')
+  end
 
+  describe 'Get #index' do
+    it 'returns a successful 200 response' do
+      get :index, format: :json
+      expect(response).to be_success
+    end
+    it 'returns all of the todos' do
+      get :index, format: :json
+      expect(Todo.all[0]).to eq(@todo)
+    end
+  end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-
   before do
-    @todo = Todo.create(description: "Go to the store to get milk and eggs.")
+    @todo = Todo.create(description: 'Go to the store to get milk and eggs.')
   end
 
   it 'has a description' do
-    expect(@todo.description).to eq("Go to the store to get milk and eggs.")
+    expect(@todo.description).to eq('Go to the store to get milk and eggs.')
   end
 
   Todo.destroy_all
